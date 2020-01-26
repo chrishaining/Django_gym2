27ca6dcd7ebe0d360b3ea86a22c96c87b771db5f
Chris <chrishaining@yahoo.co.uk>
2020-01-26 21:14:16 +0000

    adds models to admin and makes admin link to home page

--- django_gym2/gym/admin.py
+++ django_gym2/gym/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from django.contrib.admin import sites
+from gym.models import *
 # Register your models here.
+admin.site.register(Member)
+admin.site.register(Session)
+admin.site.register(Instructor)
+# reset the View Site link in the admin page (the default is to go to root, which I didn't want)
+sites.AdminSite.site_url = '/gym/'
