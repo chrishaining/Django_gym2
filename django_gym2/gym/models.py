3c00c892d74adb49980cae7e503d6b05d2b569e8
Chris <chrishaining@yahoo.co.uk>
2020-01-26 15:38:38 +0000

    session passes all tests on title and instructor

--- django_gym2/gym/models.py
+++ django_gym2/gym/models.py
@@ -4,22 +4,31 @@ from django.db import models
 class Member(models.Model):
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
-    #dob = models.CharField(max_length=255) # for ease, leave this as a string for now, though some sort of date time would be better
+    dob = models.CharField(max_length=255, default="Not given") # for ease, leave this as a string for now, though some sort of date time would be better
     age = models.IntegerField(default=0) # leave this as a direct input for now, though I could make the age dependent on dob
     weight = models.IntegerField(default=0)
     height = models.IntegerField(default=0)
-    # LOW = 'Low'
-    # MEDIUM = 'Medium'
-    # HIGH = 'High'
-    # FITNESS_LEVEL_CHOICES = [
-    #     (LOW, 'Low'),
-    #     (MEDIUM, 'Medium'),
-    #     (HIGH, 'High')
-    # ]
-    # fitness_level = models.CharField(max_length=6,
-    #     choices=FITNESS_LEVEL_CHOICES,
-    #     default=MEDIUM,)
+    LOW = 'Low'
+    MEDIUM = 'Medium'
+    HIGH = 'High'
+    FITNESS_LEVEL_CHOICES = [
+        (LOW, 'Low'),
+        (MEDIUM, 'Medium'),
+        (HIGH, 'High')
+    ]
+    fitness_level = models.CharField(max_length=6,
+        choices=FITNESS_LEVEL_CHOICES,
+        default=MEDIUM,)
     goal = models.CharField(max_length=255, default="General fitness")
-    #attendance_level = models.IntegerField() # later, this will be based on their sign-ups for sessions
+    attendance = models.IntegerField(default=0) # later, this will be based on their sign-ups for sessions
     sessions = [] # leave this as blank for now, but later I will want to add a method to add sessions.
 
+class Instructor(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    #classes_they_can_teach = []
+    profile = models.TextField()
+    
+class Session(models.Model):
+    title = models.CharField(max_length=255)
+    instructor = models.ForeignKey(Instructor, default="", on_delete=models.CASCADE)
--- django_gym2/gym/tests.py
+++ django_gym2/gym/tests.py
@@ -4,7 +4,7 @@ from gym.models import *
 # Create your tests here.
 class MemberTestCase(TestCase):
     def setUp(self):
-        self.member1 = Member.objects.create(first_name = "Ron", last_name = "Jones", age=34, weight=70, height=180, goal="Lose weight")
+        self.member1 = Member.objects.create(first_name = "Ron", last_name = "Jones", dob="01/Jan/1986", age=34, weight=70, height=180, fitness_level="High", goal="Lose weight", attendance=10)
     
     def test_member_saved(self):
         self.assertGreater(self.member1.pk, 0)
@@ -14,6 +14,9 @@ class MemberTestCase(TestCase):
 
     def test_member_last_name(self):
         self.assertEquals(self.member1.last_name, "Jones")
+
+    def test_member_dob(self):
+        self.assertEquals(self.member1.dob, "01/Jan/1986")
 
     def test_member_age(self):
         self.assertEquals(self.member1.age, 34)
@@ -27,35 +30,44 @@ class MemberTestCase(TestCase):
     def test_member_has_goal(self):
         self.assertNotEquals(self.member1.goal, "")
     
+    def test_member_attendance(self):
+        self.assertEquals(self.member1.attendance, 10)
+
     def test_member_starts_with_no_sessions(self):
         self.assertEquals(len(self.member1.sessions), 0)
 
-# class SessionTestCase(TestCase):
-#     def setUp(self):
-#         self.instructor1 = Instructor.objects.create(first_name = "Mazie", last_name = "Kinsella")
-#         self.session1 = Session.objects.create(title = "Pilates", instructor=self.instructor1)
+    def test_member_fitness_level(self):
+        self.assertEquals(self.member1.fitness_level, "High")
+
+class SessionTestCase(TestCase):
+    def setUp(self):
+        self.instructor1 = Instructor.objects.create(first_name = "Mazie", last_name = "Kinsella", profile="I love pilates!")
+        self.session1 = Session.objects.create(title = "Pilates", instructor=self.instructor1)
     
-    # def test_session_saved(self):
-    #     self.assertGreater(self.session1.pk, 0)
+    def test_session_saved(self):
+        self.assertGreater(self.session1.pk, 0)
     
-    # def test_session_name(self):
-    #     self.assertEquals(self.session1.title, "Pilates")
+    def test_session_name(self):
+        self.assertEquals(self.session1.title, "Pilates")
 
-    # def test_session_can_have_instructor(self):
-    #     self.assertEquals(self.session1.instructor.pk, self.instructor1.pk)
+    def test_session_can_have_instructor(self):
+        self.assertEquals(self.session1.instructor.pk, self.instructor1.pk)
 
-    # def test_session_instructor_first_name(self):
-    #     self.assertEquals(self.session1.instructor.first_name, "Mazie")
+    def test_session_instructor_first_name(self):
+        self.assertEquals(self.session1.instructor.first_name, "Mazie")
 
-# class InstructorTestCase(TestCase):
-#     def setUp(self):
-#         self.instructor1 = Instructor.objects.create(first_name = "Mazie", last_name = "Kinsella")
+class InstructorTestCase(TestCase):
+    def setUp(self):
+        self.instructor1 = Instructor.objects.create(first_name = "Mazie", last_name = "Kinsella", profile="I love pilates!")
     
-#     def test_instructor_saved(self):
-#         self.assertGreater(self.instructor1.pk, 0)
+    def test_instructor_saved(self):
+        self.assertGreater(self.instructor1.pk, 0)
 
-#     def test_instructor_first_name(self):
-#         self.assertEquals(self.instructor1.first_name, "Mazie")
+    def test_instructor_first_name(self):
+        self.assertEquals(self.instructor1.first_name, "Mazie")
 
-#     def test_instructor_last_name(self):
-#         self.assertEquals(self.instructor1.last_name, "Kinsella")
+    def test_instructor_last_name(self):
+        self.assertEquals(self.instructor1.last_name, "Kinsella")
+
+    def test_instructor_profile(self):
+        self.assertEquals(self.instructor1.profile, "I love pilates!")
