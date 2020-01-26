b80e3f2ee40804251b13e89ab089962034535eff
Chris <chrishaining@yahoo.co.uk>
2020-01-26 23:11:24 +0000

    adds login page that takes the user to a member_details page. Still needs log out and a way to get the member's details onto the member_details page.

--- django_gym2/gym/views.py
+++ django_gym2/gym/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from gym.models import *
-
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def index(request):
     sessions = Session.objects.all()
     instructors = Instructor.objects.all()
     return render(request, "gym/index.html", locals())
+
+# when the user logs in, they should be taken to the member_details page, which shows their personal details as well as their sessions. I am not sure how to access the Member instance (without getting all the other members, too)
+@login_required
+def member(request):
+ members = Member.objects.all()
+ return render(request, "gym/member_details.html", locals())
