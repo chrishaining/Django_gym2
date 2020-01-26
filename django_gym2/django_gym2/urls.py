e88699d6a9ee642622aed9c531fb93caec05660d
Chris <chrishaining@yahoo.co.uk>
2020-01-26 16:56:13 +0000

    creates home page with view of all sessions and instructors. also has nav bar

--- django_gym2/django_gym2/urls.py
+++ django_gym2/django_gym2/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('home/', include('gym.urls'))
-    path('admin/', admin.site.urls),
+    path('gym/', include('gym.urls')),
+    path('admin/', admin.site.urls)
 ]
--- django_gym2/gym/views.py
+++ django_gym2/gym/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from gym.models import *
+
 
 # Create your views here.
+def index(request):
+    sessions = Session.objects.all()
+    instructors = Instructor.objects.all()
+    return render(request, "gym/index.html", locals())
